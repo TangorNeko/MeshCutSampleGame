9a3355b985fef03c1ba48715e4dbbdda166253f4
nisikiori <mikan46494@gmail.com>
2021-10-08 14:09:31 +0900

    片方に頂点が2つある際のTriangleMakerの処理を仮の物から変更

--- GameTemplate/Game/TwoOnBackTriangle.cpp
+++ GameTemplate/Game/TwoOnBackTriangle.cpp
@@ -13,10 +13,25 @@ namespace Util
 		//1つ目の新頂点と対角の頂点でできた線分と、残り2つの頂点をそれぞれつないだ三角形2つが裏側の三角形
 		m_backIndexBuffer->indices.push_back((*m_newpointArray)[0]);//新頂点1
 		m_backIndexBuffer->indices.push_back(m_triangleData->vertexIndexes[m_diagonal]);//新頂点1の対角の頂点
-		m_backIndexBuffer->indices.push_back(1);//残りの頂点の1つめ(TODO:後から変更、新頂点の2つ目になるかな?)
+		m_backIndexBuffer->indices.push_back((*m_newpointArray)[1]);//残りの頂点の1つめ(TODO:後から変更、新頂点の2つ目になるかな?)
 
 		m_backIndexBuffer->indices.push_back((*m_newpointArray)[0]);//新頂点1
 		m_backIndexBuffer->indices.push_back(m_triangleData->vertexIndexes[m_diagonal]);//新頂点1の対角の頂点
-		m_backIndexBuffer->indices.push_back(2);//残りの頂点の2つめ(TODO:後から変更、backIndexesのうちm_diagonalじゃない方になりそう)
+		m_backIndexBuffer->indices.push_back(m_triangleData->vertexIndexes[BackLeftOf(m_diagonal)]);//残りの頂点の2つめ(TODO:後から変更、backIndexesのうちm_diagonalじゃない方になりそう)
+	}
+
+	uint32_t TwoOnBackTriangle::BackLeftOf(uint32_t diagonalIndex)
+	{
+		for (auto index : m_vertexIndexesPack->backVertexIndexes)
+		{
+			if (index != diagonalIndex)
+			{
+				return index;
+			}
+		}
+
+		MessageBoxA(nullptr, "一致しないインデックスを検索できませんでした。", "エラー", MB_OK);
+		std::abort();
+		return -1;
 	}
 }
--- GameTemplate/Game/TwoOnBackTriangle.h
+++ GameTemplate/Game/TwoOnBackTriangle.h
@@ -9,6 +9,13 @@ namespace Util
 		 * @brief インデックスバッファを構成する
 		*/
 		void MakeTriangles() override;
+
+		/**
+		 * @brief 裏側の頂点のうち、指定したインデックス番号でないインデックスを返す
+		 * @param diagonalIndex 指定インデックス番号(対角のインデックス番号)
+		 * @return 指定したインデックス番号でないインデックス番号
+		*/
+		uint32_t BackLeftOf(uint32_t diagonalIndex);
 	};
 }
 
