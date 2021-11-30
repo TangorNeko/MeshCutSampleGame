5a34c425dd25fda0f9fbafcf06063552f3e80898
nisikiori <mikan46494@gmail.com>
2021-11-30 14:18:52 +0900

    アニメーションがついてるので攻撃を示す仮モデルは不要なのでコメントアウト 代わりにトリガーを作成する可能性があるので完全に削除はしないでおく。

--- GameTemplate/Game/PlayerAttack.cpp
+++ GameTemplate/Game/PlayerAttack.cpp
@@ -18,10 +18,12 @@ namespace Game
 {
 	PlayerAttack::~PlayerAttack()
 	{
+		/*
 		if (m_testHitBox != nullptr)
 		{
 			DeleteGO(m_testHitBox);
 		}
+		*/
 	}
 
 	void PlayerAttack::Update(const Vector3& playerPosition, PlayerAnimationParam& animParam)
@@ -40,19 +42,23 @@ namespace Game
 			m_attackTime++;
 
 			//当たり判定があれば
+			/*
 			if (m_testHitBox != nullptr)
 			{
 				//当たり判定の座標をプレイヤーの位置にセット
 				m_testHitBox->SetPosition(playerPosition);
 			}
+			*/
 
 			//攻撃の当たり判定を作成する時間になったら
 			if (m_attackTime == TIME_ATTACK_COLLISION)
 			{
-				//当たり判定を作成
+				//TODO:トリガーの当たり判定を作成
+				/*
 				m_testHitBox = NewGO<SkinModelRender>(0);
 				m_testHitBox->Init(PATH_HITBOXMODEL);
 				m_testHitBox->SetPosition(playerPosition);
+				*/
 
 				//ボスに近ければダメージを与える
 				//TODO:ボスの種類が増えることが予想されるので基底クラスを用意する?
@@ -71,8 +77,10 @@ namespace Game
 			if (g_pad[0]->IsTrigger(BUTTON_ATTACK) && m_attackTime > TIME_COMBO_ACCEPTABLE && m_comboNum <= 2)
 			{
 				//次のコンボに移るため当たり判定は削除
+				/*
 				DeleteGO(m_testHitBox);
 				m_testHitBox = nullptr;
+				*/
 
 				//コンボ段数をインクリメント
 				m_comboNum++;
@@ -85,8 +93,10 @@ namespace Game
 			if (m_attackTime > TIME_ATTACK_END)
 			{
 				//当たり判定を削除
+				/*
 				DeleteGO(m_testHitBox);
 				m_testHitBox = nullptr;
+				*/
 
 				//コンボ段数と攻撃時間をリセット
 				m_comboNum = 0;
