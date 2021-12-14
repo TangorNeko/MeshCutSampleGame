7a1b1359ca63e25c1b0571e878b0448489211612
nisikiori <mikan46494@gmail.com>
2021-12-14 17:02:00 +0900

    足場オブジェクトの移動方向と生存時間を追加

--- GameTemplate/Game/StepObject.cpp
+++ GameTemplate/Game/StepObject.cpp
@@ -1,6 +1,12 @@
 #include "stdafx.h"
 #include "StepObject.h"
 
+namespace
+{
+	const float MOVESPEED = 10.0f;
+	const int MAX_LIFETIME = 400;
+}
+
 namespace Game
 {
 	StepObject::~StepObject()
@@ -12,16 +18,29 @@ namespace Game
 	{
 		m_skinModelRender = NewGO<SkinModelRender>(0);
 		m_skinModelRender->Init("Assets/modelData/TankMissile.tkm");
-		Quaternion qRot;
-		qRot.SetRotationDegX(90.0f);
-		m_skinModelRender->SetRotation(qRot);
 
 		return true;
 	}
 
 	void StepObject::Update()
 	{
-		m_position.z += 5.0f;
+		//移動方向への回転を計算
+		m_qRot.SetRotation(Vector3::Up, m_moveDirection);
+		
+		//移動方向へ移動
+		m_position += m_moveDirection * MOVESPEED;
+
+		//座標と回転をセット
 		m_skinModelRender->SetPosition(m_position);
+		m_skinModelRender->SetRotation(m_qRot);
+
+		//生存時間をインクリメント
+		m_lifeTime++;
+
+		//最大生存時間になったら削除
+		if (m_lifeTime == MAX_LIFETIME)
+		{
+			DeleteGO(this);
+		}
 	}
 }
--- GameTemplate/Game/StepObject.h
+++ GameTemplate/Game/StepObject.h
@@ -11,18 +11,39 @@ namespace Game
 
 		void Update() override;
 
+		/**
+		 * @brief 座標を設定する
+		 * @param position 座標
+		*/
 		void SetPosition(const Vector3& position)
 		{
 			m_position = position;
 		}
 
+		/**
+		 * @brief 座標を取得する
+		 * @return 座標
+		*/
 		const Vector3& GetPosition()
 		{
 			return m_position;
 		}
+
+		/**
+		 * @brief 移動方向を指定する
+		 * @param moveDirection 
+		*/
+		void SetMoveDirection(const Vector3& moveDirection)
+		{
+			m_moveDirection = moveDirection;
+			m_moveDirection.Normalize();
+		}
 	private:
-		Vector3 m_position =  Vector3::Zero;
-		SkinModelRender* m_skinModelRender = nullptr;
+		Quaternion m_qRot;								//モデルの回転
+		Vector3 m_moveDirection = Vector3::AxisZ;		//移動方向
+		Vector3 m_position =  Vector3::Zero;			//座標
+		SkinModelRender* m_skinModelRender = nullptr;	//スキンモデルレンダー
+		int m_lifeTime = 0;								//生存時間
 	};
 }
 
